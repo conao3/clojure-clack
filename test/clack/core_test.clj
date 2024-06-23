679b2013306528087414d5e39a91ef1d460b67a7
Naoya Yamashita <conao3@gmail.com>
2024-06-23 14:49:59 +0900

    add test-runner

--- test/clack/core_test.clj
+++ test/clack/core_test.clj
@@ -1,7 +1,7 @@
 (ns clack.core-test
-  (:require [clojure.test :refer :all]
-            [clack.core :refer :all]))
+  (:require [clojure.test :as t]
+            [clack.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest a-test
+  (t/testing "FIXME, I fail."
+    (t/is (= 0 1))))
