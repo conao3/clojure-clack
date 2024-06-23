2c53b3832c6ea220430dde48c1c7e8e0f9a1cb2e
Naoya Yamashita <conao3@gmail.com>
2024-06-23 17:18:00 +0900

    add logging

--- src/clack/system.clj
+++ src/clack/system.clj
@@ -2,17 +2,28 @@
   (:require
    [clack.component.handler :as c.handler]
    [clack.component.server :as c.server]
-   [com.stuartsierra.component :as component]))
+   [clojure.tools.logging :as logging]
+   [com.stuartsierra.component :as component]
+   [unilog.config :as unilog]))
 
 (defn- new-system []
   (component/system-map
-   :handler (c.handler/->Handler {})
+   :handler (c.handler/map->Handler {})
    :server (component/using
             (c.server/map->Jetty9Server {:opts {:join? false :port 8000}})
             [:handler])))
 
+(defonce logging-config {:level :debug
+                         :console true
+                         :overrides {"org.eclipse.jetty" :info}})
+
 (defn start []
-  (component/start (new-system)))
+  (let [system (new-system)
+        _ (unilog/start-logging! logging-config)
+        _ (logging/info "system is ready to start")
+        started-system (component/start system)
+        _ (logging/info "system is started")]
+    started-system))
 
 (defn stop [system]
   (component/stop system))
